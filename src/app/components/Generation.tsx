fd960d90a3c9ee084fa405b5435e7b910395da30
imgeekk <imgeekkk@gmail.com>
2026-01-25 21:26:22 +0530

    Landing section done. Started the Generate section

--- src/app/components/Generation.tsx
+++ src/app/components/Generation.tsx
@@ -1,5 +1,14 @@
 const Generation = () => {
-  return <div className="text-white">Generation</div>;
+  return <div id="generate" className="min-h-screen w-full text-white/90 flex items-center justify-center font-[inter-regular]">
+    <form action="" className="border w-[90%] sm:w-[50%]">
+    <input
+            type="text"
+            placeholder="What happened in today's vlog"
+            name="name"
+            className="flex h-9 w-full rounded-sm border border-zinc-800 bg-background px-3 py-2 mb-3 text-foreground shadow-sm shadow-black/5 transition-shadow placeholder:text-muted-foreground/70 focus-visible:border-[#0F3858] focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50"
+          />
+    </form>
+  </div>;
 };
 
 export default Generation;
--- src/app/components/Landing.tsx
+++ src/app/components/Landing.tsx
@@ -1,9 +1,34 @@
-const Landing= () => {
+const Landing = () => {
   return (
-    <div
-      className="h-screen overflow-hidden mask-[linear-gradient(to_top,transparent,black_40%)] [-webkit-mask-image:linear-gradient(to_top,transparent,black_40%)]"
-    >
-      <div className="min-h-screen w-full bg-[url('/bg.png')] bg-no-repeat bg-center bg-cover"></div>
+    <div className="h-screen text-white/90 font-[inter-bold] overflow-hidden mask-[linear-gradient(to_top,transparent,black_40%)] [-webkit-mask-image:linear-gradient(to_top,transparent,black_40%)]">
+      <div className="relative min-h-screen w-full flex flex-col justify-center bg-[url('/bg.png')] bg-no-repeat bg-center bg-cover">
+        <nav className="absolute top-0 w-full flex items-center justify-between p-5 sm:px-30 sm:py-5 text-lg sm:text-2xl">
+          <div className="flex items-center">
+            <img src={"/logo.svg"} className="h-6 w-6 sm:h-8 sm:w-8"></img>
+            <h1 className="pl-1">CreateHook</h1>
+          </div>
+          <button className="px-3 py-1 bg-white/30 hover:bg-white/20 transition-all duration-100 ease-in ">
+            <a href="#generate" className="text-[15px] sm:text-xl font-[inter-regular]">
+              Generate
+            </a>
+          </button>
+        </nav>
+        <header className="font-stretch-ultra-condensed flex flex-col items-center">
+          <h1 className="px-2 text-5xl sm:text-[80px] mb-5 leading-none text-center tracking-tight font-[inter-bold]">
+            Your<span className="font-[instrument]"> Vlog </span>is good.
+            <br></br>But the<span className="font-[instrument]"> Hook </span>{" "}
+            decides the<span className="font-[instrument]"> Views.</span>
+          </h1>
+          <h2 className="text-center text-md px-5 mb-5 sm:text-xl text-white/70">
+            Get natural opening lines that makes the viewers stay past the first 3 seconds.
+          </h2>
+          <button className="px-4 py-2 bg-white/30 hover:bg-white/20 transition-all duration-100 ease-in ">
+            <a href="#generate" className="text-xl font-[inter-regular]">
+              Generate
+            </a>
+          </button>
+        </header>
+      </div>
     </div>
   );
 };
